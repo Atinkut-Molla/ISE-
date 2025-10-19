df73f18c891f10913a54b5ad3566f48ef5b001a9
Atinkut-Molla <atinkut19@gmail.com>
2025-10-19 10:35:28 +0300

    Update feynman_solver.py

--- feynman_solver.py
+++ feynman_solver.py
@@ -2,7 +2,7 @@ def solve_feynman():
     A = 5
     quotient = 111111
     dividend = A * quotient
-    
+
     print("CLASSIC FEYNMAN SOLUTION")
     print(f"A = {A}")
     print(f"Dividend = {dividend}")
